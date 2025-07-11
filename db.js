10e36ee441013b9b76567bbc968a0090fb7c1817
najwa aulia <safinatunnajahnajwa@gmail.com>
2025-07-11 11:47:03 +0700

    Convert Express server to serverless function for Vercel

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mysql = require('mysql2');
 
 // Setup koneksi database
 const pool = mysql.createPool({
-  host: 'shortline.proxy.rlwy.net',
+  host: 'crossover.proxy.rlwy.net',
   user: 'root',
-  password: 'sYTiLDWEIhLaZVVMRyIclTVvaUmDaZmU',
+  password: 'ZiCBGLBGVEQQzdfYysRcLGRWhBWqKfUc',
   database: 'railway',
   port: 3306,
 });
