0fdf6e39c745b7ab48268932296022aef5b1f2fd
najwaaulia05 <aulianajwa703@gmail.com>
2025-08-24 17:59:12 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 // db.js
 import { createClient } from '@supabase/supabase-js'
 
-// pakai URL & KEY dari Supabase Project
-const supabaseUrl = 'https://jiujogorhfadkuniedvs.supabase.co'
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY // simpan di Vercel env
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
